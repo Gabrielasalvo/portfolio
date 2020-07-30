139d924bc9689d7218d56d21578aca9c356e3316
gabrielasalvo <gabrielamdesalvo@gmail.com>
2020-07-30 18:28:01 -0300

    Dia 1 do projeto do portfólio. Projeto base em andamento

---
+++ src/container/Main/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, DivMain } from './styled'
+
+
+ function Main (){
+
+    return (
+    <Container>
+        <DivMain>DIV MAE</DivMain>
+        MAIN
+       
+    </Container>
+)
+
+}
+
+export default Main
---
+++ src/container/Main/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+
+
+export const Container = styled.div `
+width:100%;
+display:grid;
+grid-template-columns: 2fr 3fr;
+grid-template-rows: 1fr 1fr;
+background-color:red;
+
+`
+
+export const DivMain = styled.div `
+width:90%;
+background-color: purple;
+
+
+`
